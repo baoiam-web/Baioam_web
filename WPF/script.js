ef02fd3766b831825ce9978240ee472bb3ec8cb7
Raw0126 <146454204+Raw0126@users.noreply.github.com>
2023-11-30 12:42:21 +0530

    Update script.js

--- WPF/script.js
+++ WPF/script.js
@@ -1,16 +1,4 @@
-var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-}
+
 
 
     const wepAccordionFaqItemHeaders = document.querySelectorAll(".wep-accordion-faq-item-header");

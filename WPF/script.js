35651080064a46dacac602e52454c07218239f3d
Raw0126 <146454204+Raw0126@users.noreply.github.com>
2023-11-30 12:51:53 +0530

    Update script.js

--- WPF/script.js
+++ WPF/script.js
@@ -10,28 +10,5 @@ var swiper = new Swiper('.blog-slider', {
       el: '.blog-slider__pagination',
       clickable: true,
     }
-
-
-    const wepAccordionFaqItemHeaders = document.querySelectorAll(".wep-accordion-faq-item-header");
-
-    wepAccordionFaqItemHeaders.forEach(wepAccordionFaqItemHeader => {
-        wepAccordionFaqItemHeader.addEventListener("click", event => {
-
-
-            const currentlyActiveWepAccordionFaqItemHeader = document.querySelector(".wep-accordion-faq-item-header.active");
-            if (currentlyActiveWepAccordionFaqItemHeader && currentlyActiveWepAccordionFaqItemHeader !== wepAccordionFaqItemHeader) {
-                currentlyActiveWepAccordionFaqItemHeader.classList.toggle("active");
-                currentlyActiveWepAccordionFaqItemHeader.nextElementSibling.style.maxHeight = 0;
-            }
-
-            wepAccordionFaqItemHeader.classList.toggle("active");
-            const wepAccordionFaqItemBody = wepAccordionFaqItemHeader.nextElementSibling;
-            if (wepAccordionFaqItemHeader.classList.contains("active")) {
-                wepAccordionFaqItemBody.style.maxHeight = wepAccordionFaqItemBody.scrollHeight + "px";
-            }
-            else {
-                wepAccordionFaqItemBody.style.maxHeight = 0;
-            }
-
-    
+   
   });
